fa22d719aaffece15bc75b06d296bbafee1328e1
Reese Laye <reese.laye@gmail.com>
2018-01-10 23:08:31 +0800

    temp !fix

--- setup-1.sh
+++ setup-1.sh
@@ -1,4 +1,12 @@
 sudo apt-get update
 sudo apt-get install python-pip -y
 pip install shadowsocks
+
+nano /etc/apt/sources.list
+deb http://ftp.cn.debian.org/debian/ jessie main non-free contrib
+deb http://ftp.uk.debian.org/debian/ jessie main non-free contrib
+sudo apt-get update
+apt-cache search gcc
+sudo apt-get install g++-4.9 -y
+
 wget -N --no-check-certificate https://raw.githubusercontent.com/reeselaye/ss-setup/master/bbr.sh && bash bbr.sh install
