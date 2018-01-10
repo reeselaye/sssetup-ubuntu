621a249b8708a8a2119f1e0cafa1ea829a8de817
Reese Laye <reese.laye@gmail.com>
2018-01-10 21:15:12 +0800

    temp

--- setup.sh
+++ setup.sh
@@ -3,8 +3,3 @@ sudo apt-get update
 sudo apt-get install python-pip -y
 pip install shadowsocks
 wget -N --no-check-certificate https://github.com/reeselaye/ss-setup/blob/master/bbr.sh && bash bbr.sh install
-
-bash bbr.sh start
-sysctl net.ipv4.tcp_available_congestion_control
-sudo ssserver -p 443 -k 11111111 -m aes-256-cfb --user nobody -d start
-sudo ssserver -d stop
