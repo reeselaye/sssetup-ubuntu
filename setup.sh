1122f0a53ca11fdee429041cf0b036e942cf5f56
Reese Laye <reese.laye@gmail.com>
2018-01-10 21:10:53 +0800

    + auto yes when apt-get install

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 
 sudo apt-get update
-sudo apt-get install python-pip
+sudo apt-get install python-pip -y
 pip install shadowsocks
 wget -N --no-check-certificate https://github.com/reeselaye/ss-setup/blob/master/bbr.sh && bash bbr.sh install
 
