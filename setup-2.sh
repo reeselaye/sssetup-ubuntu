6f7d2aaa62f0f890d2649d44b78f7785290aaf75
李倍存 <libeicun@foxmail.com>
2018-01-11 11:14:12 +0800

    ! fix

--- setup-2.sh
+++ setup-2.sh
@@ -1,4 +1,4 @@
 sysctl net.ipv4.tcp_available_congestion_control
 
-sudo ssserver -p 443 -k 11111111 -m aes-128-gcm --user nobody -d start
+sudo ssserver -p 443 -k 11111111 -m rc4-md5 --user nobody -d start
 
