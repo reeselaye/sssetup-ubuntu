09e588bba8cc114f5ef7fd062de3346a6d4235d4
Reese Laye <reese.laye@gmail.com>
2019-09-19 19:34:34 +0800

    Update setup-1.sh

--- setup-1.sh
+++ setup-1.sh
@@ -1,8 +1,3 @@
 sudo apt-get update
 sudo apt-get install python-pip -y
 sudo pip install shadowsocks
-
-wget --no-check-certificate https://raw.githubusercontent.com/reeselaye/ss-setup_debian_ubuntu/master/bbr.sh
-sudo chmod +x bbr.sh
-sudo ./bbr.sh
-
